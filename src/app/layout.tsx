dce10a5c2aa2b56d0d280c2ad7ab583d026a814a
Mani Meskartehrani <mani.tehrani.work@gmail.com>
2024-10-12 20:37:12 -0700

    commit message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Triangle Landing Page",
-  description: "Triangle Technology",
+  description: "Triangle Technology 2",
 };
 
 export default function RootLayout({
